ab28f375fe07c3056de21775e0f841a009a462b1
PUNEETH A <PUNEETH A@DESKTOP-IU1AF6N>
2021-10-22 03:29:05 -0700

    try catchblock commit

---
+++ dxcjava/src/exceptions/VoteElgibilityException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class VoteElgibilityException extends Exception {
+
+		public VoteElgibilityException(String message) {
+			super("ineligible to vote this year");
+		}
+
+
+
+	}
+
+
