dd0fb9a076807633cef4b8cc811f318658d5d43d
PUNEETH A <PUNEETH A@DESKTOP-IU1AF6N>
2021-11-11 01:40:41 -0800

    object class- commit

---
+++ dxcjava/src/objectclass/Animal.java
@@ -0,0 +1,22 @@
+package objectclass;
+
+public class Animal {
+	
+	void eat( ) {
+		System.out.println("Animal is eating");
+	}
+	
+}
+
+class Dog extends Animal {
+	
+	void eat() {
+		System.out.println("Dog is eating");
+	}
+	
+	public static void main(String[] args) {
+		Animal a = new Dog();
+		a.eat();
+		
+	}
+}
---
+++ dxcjava/src/objectclass/Util.java
@@ -0,0 +1,16 @@
+package objectclass;
+
+public class Util {
+	/**
+	 * this class has all utility methods
+	 * @author Admin
+	 *
+	 */
+	public static String COMPANY_NAME = "dxc";
+
+	public static int incrementByTen(int no) {
+		return no + 10;
+	}
+
+}
+
