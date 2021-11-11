f8052fd88d27e9a5f42e69e1df18f581d4503a96
PUNEETH A <PUNEETH A@DESKTOP-IU1AF6N>
2021-11-11 01:34:11 -0800

    basics- debug demo

--- dxcjava/src/basics/DebugDemo.java
+++ dxcjava/src/basics/DebugDemo.java
@@ -6,15 +6,18 @@ public class DebugDemo {
 		System.out.println("DebugDemo---main");
 		Calculator calc = new Calculator();
 		int sum = calc.add(10,20);
-		
 		System.out.println("The sum is --"+sum);
+		
+		Student stud = new Student("Rohith", 34);
+		Student myStudent = new Student();
+		Student yourStudent = new Student("Virat");
 		
 		for(int a=0; a<5; a++) {
 			int f = a + 5 * 3;
 			System.out.println("The no is --"+f);
 		}
 		
-		//throw new NullPointerException();
+		throw new NullPointerException();
 	}
 
 }
