4110424377dbd870bccb67ace6013bb0243b19ff
PUNEETH A <PUNEETH A@DESKTOP-IU1AF6N>
2021-11-11 02:24:48 -0800

    student object

---
+++ dxcjava/src/basics/Student.java
@@ -0,0 +1,38 @@
+package basics;
+
+import objectclass.Util;
+
+public class Student {
+	String name;
+	int age; 
+	public Student() {
+		
+	}
+	public Student(String name) {
+		this.name = name;
+	}
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age + 2;
+		this.age = Util.incrementByTen(age);
+	}
+	static String COLLEGE_NAME = "IIT";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+			
+}
+  
+
+
+
